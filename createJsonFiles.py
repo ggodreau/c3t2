a88d5e3f5c4506be3c941d8f9592163cd8105f7b
ggodreau <godreau@gmail.com>
2016-11-07 00:31:12 -0600

    added input and output file paths and improved error handling and debug

--- createJsonFiles.py
+++ createJsonFiles.py
@@ -1,11 +1,32 @@
 #!/usr/bin/env python
 # reducer.py
 
+"""
+Before running, make sure you set the scripts folder and output
+folder paths below.
+Your Mapper.py and Reducer.py should both be within the scripts
+folder of your s3 account and must be named 'Mapper.py' and 
+'Reducer.py' (case sensitive).
+The output folder is where the map/reduce resultant files will
+be placed.
+"""
+
+## SCRIPT AND OUTPUT FOLDER PATHS MUST BE
+## CHANGED PRIOR TO RUNNING THIS SCRIPT
+
+scriptsPath =  "s3://[S3 Bucket]/[Scripts Folder]/"
+outputPath = "s3://[S3 Bucket]/[EMR Output Folder]/"
+
+
+
+
 import sys
 import re
 import os
 
+
 def main():
+    scriptsPathCheck()
     inputFileName = getUserInputFile()
     outputFileName = getUserOutputFile()
     if not detectExistingOutputFile(outputFileName):
@@ -24,6 +45,7 @@ def main():
                         sys.exit()
                     # Parse the elements out of the line
                     linesplit=line.split("/")
+                    ccMainNumber = linesplit[4]
                     segmentnumber = linesplit[6]
                     filerange = linesplit[8].rstrip()
 
@@ -37,17 +59,18 @@ def main():
                     outfile.write ("\"Jar\": \"/home/hadoop/contrib/streaming/hadoop-streaming.jar\",\n")
                     outfile.write ("\"Args\":\n")
                     outfile.write ("[\n")
-                    outfile.write ("\"-files\", \"s3://[S3 Bucket]/[Scripts Folder]/Mapper.py,s3://[S3 Bucket]/[Scripts Folder]/Reducer.py\",\n")
+                    outfile.write ("\"-files\", " + "\"" + scriptsPath + "Mapper.py," + scriptsPath + "Reducer.py\",\n")
                     outfile.write ("\"-mapper\", \"Mapper.py\",\n")
                     outfile.write ("\"-reducer\", \"Reducer.py\",\n")
-                    outfile.write ("\"-input\", \"s3://aws-publicdatasets/common-crawl/parse-output/segment/" + segmentnumber + "/" + filerange + "\",\n")
-                    outfile.write ("\"-output\", \"s3://[S3 Bucket]/[Script Output Folder]/RR_1_" + segmentnumber + "_" + filerange + "\",\n")
+                    outfile.write ("\"-input\", \"s3://commoncrawl/crawl-data/" + ccMainNumber + "/segments/" + segmentnumber + "/wet/" + filerange + "\",\n")
+                    outfile.write ("\"-output\"" + ", " + "\"" + outputPath + segmentnumber + "_" + filerange.split("-")[3] + "\"" + ",\n")
                     outfile.write ("\"-inputformat\", \"SequenceFileAsTextInputFormat\"\n")
                     outfile.write ("]\n")
                     outfile.write ("}")
                     # Print the JSON file footer markup
                     outfile.write("]\n")
                     lineCounter += 1
+                    print ccMainNumber
                 exitMessage()
 
 
@@ -72,6 +95,14 @@ def detectExistingOutputFile(file):
 
 def exitMessage():
     print "\nSuccessfully wrote", lineCounter, "lines to output file:", outputFileName
+
+def scriptsPathCheck():
+    if scriptsPath == "s3://[S3 Bucket]/[Scripts Folder]/":
+	print "You need to open and edit the script path of createJsonFiles.py file prior to running. Exiting..."
+        sys.exit()
+    elif outputPath == "s3://[S3 Bucket]/[EMR Output Folder]/":
+        print "You need to open and edit the output path of createJsonFiles.py file prior to running. Exiting..."
+        sys.exit()
 
 if __name__ == "__main__":
         main()
