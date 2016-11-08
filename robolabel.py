f5791ce420beb0f22ea06e43b5188c9f0b2087eb
ggodreau <godreau@gmail.com>
2016-11-08 16:40:48 -0600

    added weight dictionary

--- robolabel.py
+++ robolabel.py
@@ -21,6 +21,7 @@ def main():
 #    print df.ix[:,colIndex.get(coltest[0])]
 
     sumCol(coltest)
+    setWeight()
 #    for i, j, k in zip(df['id'], df['googleperunc'], df['samsunggalaxy']):
 #        df['newcol'] = i+j+k
 #    print df
@@ -43,6 +44,7 @@ def main():
     sys.exit()
 
 def getUserInputFile():
+    global file
     file = raw_input("Enter input file (leave blank for './concatenated_factors'): ")
     if file == "":
         file = "./concatenated_factors.csv"
@@ -55,14 +57,67 @@ def sumCol(colnames):
     for i in range(0,len(colnames)):
         #print colIndex.get(colnames[i])
         colIndiciesToParse.append(colIndex.get(colnames[i]))
-    print colIndiciesToParse
+    #print colIndiciesToParse
 
     for i in colIndiciesToParse:
         if i == 0:
             df['newcolname'] = df.ix[:,i]
         if i > 0:
             df['newcolname'] = df['newcolname'] + df.ix[:,i]
-    print df['newcolname']
+    #print df['newcolname']
+
+
+def getWeight(index):
+    pass
+def setWeight():
+    df = pd.read_csv(file, delimiter=',')
+    weightList = [0] * len(df.columns.values)
+    weightDict = {}
+    for i, j in zip(df.columns.values, weightList):
+        weightDict[i] = j
+
+    # device
+    weightDict['iphone'] = 0
+    weightDict['samsunggalaxy'] = 0
+
+    # OS
+    weightDict['ios'] = 0
+    weightDict['googleandroid'] = 0
+
+    # camera
+    weightDict['iphonecampos'] = 10
+    weightDict['samsungcampos'] = 10
+    weightDict['iphonecamneg'] = -10
+    weightDict['samsungcamneg'] = -10
+    weightDict['iphonecamunc'] = 1
+    weightDict['samsungcamunc'] = 1
+
+    # display
+    weightDict['iphonedispos'] = 10
+    weightDict['samsungdispos'] = 10
+    weightDict['iphonedisneg'] = -10
+    weightDict['samsungdisneg'] = -10
+    weightDict['iphonedisunc'] = 1
+    weightDict['samsungdisunc'] = 1
+
+    # device performance
+    weightDict['iphoneperpos'] = 10
+    weightDict['samsungperpos'] = 10
+    weightDict['iphoneperneg'] = -10
+    weightDict['samsungperneg'] = -10
+    weightDict['iphoneperunc'] = 1
+    weightDict['samsungperunc'] = 1
+
+    # OS performance
+    weightDict['iosperpos'] = 10
+    weightDict['googleperpos'] = 10
+    weightDict['iosperneg'] = -10
+    weightDict['googleperneg'] = -10
+    weightDict['iosperunc'] = 1
+    weightDict['googleperunc'] = 1
+
+    print weightDict
+
 
 """
 def sumCol(alpha, colnames, newcolname):
